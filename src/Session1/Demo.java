6622933feb6f0305869b5d2de788b335c4e9c674
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-03-07 11:51:05 +0700

    demo s1

--- src/Session1/Demo.java
+++ src/Session1/Demo.java
@@ -1,9 +1,28 @@
 package Session1;
 
 public class Demo {
+    int age;
+    static int  abc = 100;
+    final static int xyz = 50;
 
     public static void main(String[] args){
+        int x = 100;
         System.out.println("Hello T1807M");
+        plus();
+        Demo mainObject;
+        mainObject = new Demo();
+
+        System.out.println(mainObject.age);
+        System.out.println(Demo.abc);
+        Demo.abc = 20;
+        System.out.println(Demo.abc);
+        System.out.println(Demo.xyz);
+    }
+
+    public static void plus(){
+        int x  = 10;
+        int y = 20;
+        System.out.println(x+y);
     }
 
 }
